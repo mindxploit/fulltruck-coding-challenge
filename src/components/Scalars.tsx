d7389280ad1814154d3eb027f698ef59c3d3f3a6
mindxploit <bobbonicola@gmail.com>
2024-07-13 16:23:25 +0200

    Overview scalars finished

--- src/components/Scalars.tsx
+++ src/components/Scalars.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { PieChart } from '@mui/x-charts/PieChart'
-import { Box, Typography } from '@mui/material'
+import { Box, Card, CardContent, Divider, Stack, Typography } from '@mui/material'
+import PersonIcon from '@mui/icons-material/Person'
+import LocalShippingIcon from '@mui/icons-material/LocalShipping'
 
 interface Scalars {
   active_carriers: number
@@ -37,52 +39,106 @@ const ScalarsPieChart: React.FC<ScalarsPieChartProps> = ({ data }) => {
   ]
 
   return (
-  <>
-      <Typography variant='h3'>Overview</Typography>
-    <Box display="flex">
-      <Box>
-        <PieChart
-          height={300}
-          width={400}
-          colors={revenueMarginData.map((d) => d.color)}
-          series={[
-            {
-              data: revenueMarginData.map((d) => ({ ...d, color: d.color })),
-              innerRadius: 30,
-              outerRadius: 100,
-              paddingAngle: 5,
-              cornerRadius: 5,
-              startAngle: 0,
-              endAngle: 360,
-              cx: 150,
-              cy: 150,
-            },
-          ]}
-        />
-      </Box>
+    <>
+      <Typography variant="h3">Overview</Typography>
+      <Card sx={{ width: 'fit-content' }}>
+        <CardContent>
+          <Stack direction={'row'} spacing={3} divider={<Divider orientation="vertical" flexItem />}>
+            <Box>
+              <Stack
+                direction={'row'}
+                justifyContent={'space-around'}
+                spacing={3}
+                divider={<Divider orientation="vertical" flexItem />}
+              >
+                <Box>
+                  <Typography variant="overline">Total revenue</Typography>
+                  <Typography variant="h4">{'€' + data.total_revenue}</Typography>
+                </Box>
 
-      <Box>
-        <PieChart
-          height={300}
-          width={470}
-          colors={orderData.map((d) => d.color)}
-          series={[
-            {
-              data: orderData.map((d) => ({ ...d, color: d.color })),
-              innerRadius: 30,
-              outerRadius: 100,
-              paddingAngle: 5,
-              cornerRadius: 5,
-              startAngle: 0,
-              endAngle: 360,
-              cx: 150,
-              cy: 150,
-            },
-          ]}
-        />
-      </Box>
-    </Box>
-  </>
+                <Box>
+                  <Typography variant="overline">Average order</Typography>
+                  <Typography variant="h4">{'€' + data.avg_order_revenue.toFixed(2)}</Typography>
+                  <Typography variant="caption">{`Margin of € ${data.avg_order_margin_abs.toFixed(
+                    2
+                  )} (${data.average_margin_perc.toFixed(2)} %)`}</Typography>
+                </Box>
+              </Stack>
+
+              <PieChart
+                height={300}
+                width={400}
+                colors={revenueMarginData.map((d) => d.color)}
+                series={[
+                  {
+                    data: revenueMarginData.map((d) => ({ ...d, color: d.color })),
+                    innerRadius: 30,
+                    outerRadius: 100,
+                    paddingAngle: 5,
+                    cornerRadius: 5,
+                    startAngle: 0,
+                    endAngle: 360,
+                    cx: 150,
+                    cy: 150,
+                  },
+                ]}
+              />
+            </Box>
+
+            <Box display={'flex'} flexDirection={'column'} justifyContent={'space-between'}>
+              <Stack
+                direction={'row'}
+                justifyContent={'space-around'}
+                divider={<Divider orientation="vertical" flexItem />}
+              >
+                <Box>
+                  <Typography variant="overline">Total orders</Typography>
+                  <Typography variant="h4">{data.total_order_count}</Typography>
+                </Box>
+                <Stack direction={'row'} spacing={3}>
+                  <Box>
+                    <Typography variant="overline">Active clients</Typography>
+                    <Stack direction={'row'} alignItems={'center'} spacing={1}>
+                      <PersonIcon />
+                      <Typography variant="h6">{data.active_clients}</Typography>
+                    </Stack>
+                  </Box>
+
+                  <Box>
+                    <Typography variant="overline">Active carriers</Typography>
+                    <Stack direction={'row'} alignItems={'center'} spacing={1}>
+                      <LocalShippingIcon />
+                      <Typography variant="h6">{data.active_carriers}</Typography>
+                    </Stack>
+                  </Box>
+                </Stack>
+              </Stack>
+
+              <Box>
+                <PieChart
+                  height={300}
+                  width={470}
+                  colors={orderData.map((d) => d.color)}
+                  series={[
+                    {
+                      data: orderData.map((d) => ({ ...d, color: d.color })),
+                      innerRadius: 30,
+                      outerRadius: 100,
+                      paddingAngle: 5,
+                      cornerRadius: 5,
+                      startAngle: 0,
+                      endAngle: 360,
+                      cx: 150,
+                      cy: 150,
+                    },
+                  ]}
+                />
+              </Box>
+            </Box>
+          </Stack>
+        </CardContent>
+      </Card>
+    </>
   )
 }
 
